cddb9a5d60d34720bcb88221510f5ae42dfeff24
friskaim7 <friska.intan2001@gmail.com>
2021-10-12 15:30:40 +0700

    refactor(Create Timer): delete unnecessary comments

--- backend/api/models/timerModel.js
+++ backend/api/models/timerModel.js
@@ -9,9 +9,7 @@ module.exports = {
             status = timer.status,      // isRunning
             title = timer.title,
             start_time = timer.start_time,  // startTime
-            // current_time = timer.current_time,
             last_pause = timer.last_pause,  // puaseTime
-            // last_delay = timer.last_delay,
             last_continue = timer.last_continue,    // continueTime
             delays = timer.delays   // timeDelays
         }
